9ad86ece9d3b44e295b01eb7513e88d33bf0a4cd
jomaskill <jomasjoao@gmail.com>
2020-02-28 13:28:43 -0300

    configurações

---
+++ Micellaneous/FlippingBits.php
@@ -0,0 +1,32 @@
+<?php
+
+//Testes de entrada
+$n = "3\n0\n802743475\n35601423";
+echo flippingBits($n);
+
+
+
+// Complete the flippingBits function below.
+function flippingBits($n) {
+
+    $n = explode("\n", $n);
+    $nLinhas = $n[0];
+
+    $sum = [];
+    $array = [];
+
+
+    for($j=1 ; $j <= $nLinhas ; $j++){
+        $sum[$j-1] = 0;
+        for($i=31 ; $i >= 0 ;$i--){
+            if($n[$j] >= pow(2, $i)) {
+                $n[$j] -= pow(2, $i);
+                $array[$i] = 0;
+            }
+            else $array[$i] = 1;
+            if($array[$i] == 1) $sum[$j-1] += pow(2, $i);
+        }
+    }
+    $sum = implode("\n", $sum);
+    return $sum;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Hacker-Rank
-# Hacker-Rank
+
+Vou deixar aqui todos os códigos feitos para resolverem os desafio do HackerRank
+
+todos eles em PHP
