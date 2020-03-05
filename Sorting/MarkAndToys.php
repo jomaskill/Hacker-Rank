13881cb019652ea9f6e35c25316f01fdb4e4fb40
jomaskill <jomasjoao@gmail.com>
2020-03-05 14:47:41 -0300

    exercicio Mark and Toys

---
+++ Sorting/MarkAndToys.php
@@ -0,0 +1,16 @@
+<?php
+
+// Complete the maximumToys function below.
+function maximumToys($prices, $k) {
+
+    $numberOfToys=0;
+
+    while($k >= min($prices)){
+        $k -=  min($prices);
+        $indice = array_search(min($prices), $prices);
+        unset($prices[$indice]);
+        $numberOfToys++;
+    }
+
+    return $numberOfToys;
+}
